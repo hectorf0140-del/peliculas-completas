2abc52b0b13901f25877b528fb9dfaf36e166ba9
hectorf0140-del <hectorf0.14.0@gmail.com>
2026-07-26 16:40:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ⚠️ Completá estos dos valores antes de publicar el sitio.
 const CONFIG = {
-  TMDB_API_KEY: "TU_TMDB_API_KEY_AQUI",
+  TMDB_API_KEY: "31a09ff792f32fb3ad8d78fdb8e8def5",
   GOOGLE_CLIENT_ID: "TU_GOOGLE_CLIENT_ID_AQUI.apps.googleusercontent.com"
 };
