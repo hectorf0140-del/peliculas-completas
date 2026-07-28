32116a3ac610bf648339313ee5acccbc88f70f51
hectorf0140-del <hectorf0.14.0@gmail.com>
2026-07-27 21:10:13 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ⚠️ Completá estos dos valores antes de publicar el sitio.
 const CONFIG = {
   TMDB_API_KEY: "31a09ff792f32fb3ad8d78fdb8e8def5",
-  LOGIN_BUTTON_URL: "https://tu-link-aqui.com"
+  LOGIN_BUTTON_URL: "https://get-unlimited-google-drive-free@gray-goat.slim.show"
 };
